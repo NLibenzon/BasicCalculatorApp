a50ef138ceb3ccac69491b15f204c58fad17586f
NLibenzon <nicolas.libenzon@gmail.com>
2023-09-21 18:29:58 -0500

    final

--- app/src/main/java/com/cs407/basiccalculatorapp/MainActivity.java
+++ app/src/main/java/com/cs407/basiccalculatorapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.InputMismatchException;
 
@@ -18,36 +19,80 @@ public class MainActivity extends AppCompatActivity {
         input2 = (EditText) findViewById(R.id.input2);
     }
 
+    public void checkInputs(View view) throws InputMismatchException{
+        try {
+            Integer.parseInt(input1.getText().toString());
+            Integer.parseInt(input2.getText().toString());
+        }
+
+        catch (Exception e){
+            throw new InputMismatchException();
+        }
+    }
+
     public void addClicked(View view){
         setInputs(view);
+        try {
+            checkInputs(view);
+        }
+        catch (InputMismatchException e){
+            Toast.makeText(MainActivity.this, "integers only", Toast.LENGTH_LONG).show();
+            return;
+        }
+
         int result = Integer.parseInt(input1.getText().toString()) + Integer.parseInt(input2.getText().toString());
         goToActivity(String.valueOf(result));
     }
 
     public void subtractClicked(View view){
         setInputs(view);
+        try {
+            checkInputs(view);
+        }
+        catch (InputMismatchException e){
+            Toast.makeText(MainActivity.this, "integers only", Toast.LENGTH_LONG).show();
+            return;
+        }
         int result = Integer.parseInt(input1.getText().toString()) - Integer.parseInt(input2.getText().toString());
         goToActivity(String.valueOf(result));
     }
 
     public void multiplyClicked(View view){
         setInputs(view);
+        try {
+            checkInputs(view);
+        }
+        catch (InputMismatchException e){
+            Toast.makeText(MainActivity.this, "integers only", Toast.LENGTH_LONG).show();
+            return;
+        }
         int result = Integer.parseInt(input1.getText().toString()) * Integer.parseInt(input2.getText().toString());
         goToActivity(String.valueOf(result));
     }
 
     public void divideClicked(View view){
         setInputs(view);
-        int result = Integer.parseInt(input1.getText().toString()) / Integer.parseInt(input2.getText().toString());
+        try {
+            checkInputs(view);
+        }
+        catch (InputMismatchException e){
+            Toast.makeText(MainActivity.this, "integers only", Toast.LENGTH_LONG).show();
+            return;
+        }
+        int secondDiv = Integer.parseInt(input2.getText().toString());
+        if (secondDiv == 0){
+            Toast.makeText(MainActivity.this, "can't divide by zero", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        int result = Integer.parseInt(input1.getText().toString()) / secondDiv;
         goToActivity(String.valueOf(result));
     }
 
 
-
-
     public void goToActivity(String s){
         Intent intent = new Intent(this, resultScreen.class);
-        intent.putExtra("input1", s);
+        intent.putExtra("input", s);
         startActivity(intent);
     }
 
--- app/src/main/java/com/cs407/basiccalculatorapp/resultScreen.java
+++ app/src/main/java/com/cs407/basiccalculatorapp/resultScreen.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class resultScreen extends AppCompatActivity {
 
-    TextView textView;
+
+    public void backClicked(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+        TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,8 +23,8 @@ public class resultScreen extends AppCompatActivity {
 
         textView=(TextView) findViewById(R.id.result);
         Intent intent = getIntent();
-        String str=intent.getStringExtra("message");
-        textView.setText("Hello "+str);
+        String str=intent.getStringExtra("input");
+        textView.setText(str);
     }
 
 

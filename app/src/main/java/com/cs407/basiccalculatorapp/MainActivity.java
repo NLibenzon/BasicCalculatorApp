a1878b3f339bcea6b1f33e869065acbf9f28e26d
NLibenzon <nicolas.libenzon@gmail.com>
2023-09-21 17:44:15 -0500

    step 20

--- app/src/main/java/com/cs407/basiccalculatorapp/MainActivity.java
+++ app/src/main/java/com/cs407/basiccalculatorapp/MainActivity.java
@@ -2,9 +2,26 @@ package com.cs407.basiccalculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void addClicked(View view){
+        EditText input1 = (EditText) findViewById(R.id.input1);
+        EditText input2 = (EditText) findViewById(R.id.input2);
+        goToActivity(input1.getText().toString(), input2.getText().toString());
+
+    }
+
+    public void goToActivity(String s, String b){
+        Intent intent = new Intent(this, resultScreen.class);
+        intent.putExtra("input1", s);
+        intent.putExtra("input2", b);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/cs407/basiccalculatorapp/resultScreen.java
@@ -0,0 +1,18 @@
+package com.cs407.basiccalculatorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class resultScreen extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_screen);
+    }
+
+
+}
